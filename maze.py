7508b53967e2728e571dac30b4be884e9d04bf80
Dave Finch <davef21370@yahoo.co.uk>
2012-12-18 10:43:03 +0000

    Update maze.py

--- maze.py
+++ maze.py
@@ -3,8 +3,13 @@ import Image
 
 # Set the size of the maze.
 # These must be odd integers of 7 or above.
-xSize = 25
-ySize = 25
+xSize = 67
+ySize = 35
+
+# Set the maximum wall length.  This avoids overly long
+# walls on larger mazes.
+# The smaller the value, the greater possible solutions.
+maxWall = 13
 
 # Create a list to store the maze data.
 # 0 denotes a walkway, 1 is a wall, start with no walls.
@@ -22,20 +27,11 @@ for i in range(0,xSize):
     maze[(xSize*(ySize-1))+i] = 1
 # Left and right.
 for i in range(1,ySize-1):
-    maze[ySize*i]=1
-    maze[(i*xSize)+(xSize-1)] = 1
+    maze[xSize*i]=1
+    maze[(xSize*i)+(xSize-1)] = 1
 
 # Create the maze.
-loop = True
-while loop:
-
-    # Check to see if we have any starting points left.
-    loop = False
-    for i in range(xSize):
-        for j in range(ySize):
-            if maze[(j*xSize)+i] == 2: loop = True
-    # If not, the maze is finished so exit the loop.
-    if loop == False: break
+while 2 in maze:
 
     # Pick a random starting point.
     x = randint(1,(xSize-1)/2)*2
@@ -45,45 +41,54 @@ while loop:
     # If the point hasn't already been used pick a random direction
     # and move that way until we hit a wall.
     if c == 2:
+        mwc = 0
         r = randint(0,3)
         if r == 0:
             while c != 1:
                 maze[(y*xSize)+x] = 1
                 y -= 1
                 c = maze[(y*xSize)+x]
+                mwc += 1
+                if mwc == maxWall: break
         if r == 1:
             while c != 1:
                 maze[(y*xSize)+x] = 1
                 x += 1
                 c = maze[(y*xSize)+x]
+                mwc += 1
+                if mwc == maxWall: break
         if r == 2:
             while c != 1:
                 maze[(y*xSize)+x] = 1
                 y += 1
                 c = maze[(y*xSize)+x]
+                mwc += 1
+                if mwc == maxWall: break
         if r == 3:
             while c!= 1:
                 maze[(y*xSize)+x] = 1
                 x -= 1
                 c = maze[(y*xSize)+x]
+                mwc += 1
+                if mwc == maxWall: break
 
 # Set this to True to print the maze to screen.
 if True:
     for i in range(0,ySize):
         s=""
         for j in range(0,xSize):
-            k = maze[(j*ySize)+i]
+            k = maze[(i*xSize)+j]
             if k == 1:s=s+"@"
             else:s=s+" "
         print s
 
-# Set this to True to save the maze as a JPEG.
-if False:
+# Set this to True to save the maze as a PNG.
+if True:
     mPic = Image.new("RGB",(xSize,ySize),0)
     pix = mPic.load()
     for i in range(0,ySize):
         for j in range(0,xSize):
-            col = maze[(ySize*i)+j]*255
+            col = maze[(i*xSize)+j]*255
             col = 255-col
             pix[j,i] = col,col,col
-    mPic.save("maze.jpg")
+    mPic.save("maze.png")
